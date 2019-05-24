e3a9a40a4897ddad1f3eef19f31eb804271ce6ed
squanchy11 <philipp.steigler@mailbox.org>
2019-05-24 20:15:17 +0200

    implementation in python

--- main.py
+++ main.py
@@ -8,77 +8,121 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
+import random
+
 motor_grabber = Motor(Port.D)
 motor_l = Motor(Port.B)
 motor_r = Motor(Port.C)
 robot = DriveBase(motor_l, motor_r, 56, 114) # wheel diameter (mm) | axle track (mm)
 color_sensor = ColorSensor(Port.S1)
-#ultrasonic = UltrasonicSensor(Port.S4)
+ultrasonic = UltrasonicSensor(Port.S4)
 
-collision_threshhold = 400  # mm
-drive_speed = 100           # mm/s
+collision_threshhold = 100  # mm
+drive_speed = 80            # mm/s
 rotation_speed = 45         # deg/s
-calibration_surface = -1
 myColor = -1                # color to collect
 dropZoneColor = -1          # color of surface to drop cubes
+calibration_surface = -1
 
-#don't crash into obstacles
+# avoid objects and other mindstorms
 def collision_avoidance():
     dist = ultrasonic.distance()
     if dist < collision_threshhold:
-        robot.drive_time(0, 45, 500)
-        collision_avoidance() # check if collision free
 
-#grab cubes
-def close_grabber():
-    motor_grabber.run_target(5, 110, Stop.HOLD, False)
-    returnToHome()
+        robot.stop()
+        # turn left or right
+        rand = random.randint(0, 1)
+        if rand == 0:
+            robot.drive_time(40, -90, 1400)
+        else:
+            robot.drive_time(40, 90, 1400)
 
-#release cubes
-def open_grabber():
-    motor_grabber.run_target(5, -110, Stop.HOLD, False)
-    robot.drive_time((-1*drive_speed), 0, 1000)
-
-#navigate home
-def returnToHome():
-    while True:
+        # check again after turning
         collision_avoidance()
-        if check_color_surface():
-            #if robo found home and dropped cube, exit and continue with main routine
-            break
-        robot.drive_time(drive_speed, 90, 1000)
-        brick.display.clear()
-        robot.drive(drive_speed, 0)
 
-#check color of surface to drop item
-#assumes color sensor faces downwards to ground - otherwise install 2nd sensor!
-def check_color_surface():
-    if color_sensor.color() != calibration_surface:
-        robot.stop()
-        if color_sensor.color() == dropZoneColor:
-            open_grabber()
-            return True
+        # drive away from object
+        robot.drive_time(drive_speed, 0, 1000)
+
+# close the grabber to collect an item
+def close_grabber():
+    motor_grabber.run_until_stalled(100, Stop.HOLD)
+
+# open the grabber to drop an item
+def open_grabber():
+    motor_grabber.run_until_stalled(-100, Stop.HOLD)
+
+def make_turn():
+    turn = random.randint(0, 50)
+    if turn == 0:
+        # decide left or right turn
+        rand = random.randint(0,1)
+        if rand == 0:
+            robot.drive_time(drive_speed, 90, random.randint(400,1500))
         else:
-            #move on and search drop zone
-            robot.drive_time((-1*drive_speed), 0, 1000)
-            return False
+            robot.drive_time(drive_speed, -90, random.randint(400,1500))
 
-#check color of cube and start grabbing or move away
-def check_color_brick():
-    if color_sensor.color() != calibration_surface:
-        robot.stop()
-        if color_sensor.color() == myColor:
-            close_grabber()
-        else:
-            #move away from object
-            robot.drive_time((-1*drive_speed), 0, 1000)
+# initialize color sensor
+myColor = Color.BLACK       # color of zone to collect items
+dropZoneColor = Color.RED   # color of zone to drop items
 
-#main program
-myColor = color_sensor.color() # set my color to collect
-dropZoneColor = Color.BLUE # set color of my home surface
+# initialize state of grabber
+loaded = False
+open_grabber()
+
+# main routine
 while True:
-    collision_avoidance()
-    check_color_brick()
-    robot.drive_time(drive_speed, 90, 1000) # continue here after returnToHome
-    brick.display.clear()
-    robot.drive(drive_speed, 0)
+
+    # search for pick-up zone and grab an item
+    while loaded == False:
+        # drive around and look for pick-up zone
+        while True:
+            collision_avoidance()
+            if color_sensor.color() != calibration_surface:
+                robot.stop()
+                    # if zone is detacted stop driving around
+                    if color_sensor.color() == myColor:
+                        break
+
+            # start/continue driving ..
+            robot.drive(drive_speed, 0)
+            wait(100)
+
+            # .. eventually make a turn
+            make_turn()
+
+        # stop and grab an item
+        robot.stop()
+        close_grabber()
+
+        # move away from pick-up zone
+        robot.drive_time((-1*drive_speed), 0, 4000)
+        robot.drive_time(50, -90, 2000)
+        loaded = True
+    
+    # search for drop zone and drop the item
+    while loaded == True:
+        # drive around and look for drop zone
+        while True:
+            collision_avoidance()
+            if color_sensor.color() != calibration_surface:
+                robot.stop()
+                # if zone is detacted stop driving around
+                    if color_sensor.color() == dropZoneColor:
+                        break
+
+            # start/continue driving ..
+            robot.drive(drive_speed, 0)
+            wait(100)
+
+            # .. eventually make a turn
+            make_turn()
+        
+        # stop and drop an item
+        robot.stop()
+        open_grabber()
+
+        # move away from drop zone
+        robot.drive_time((-1*drive_speed), 0, 4000)
+        robot.drive_time(50, -90, 2000)
+        loaded = False
+        
